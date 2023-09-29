2b2549c3e826e1086df054feedf6e664c4b0bcf8
Ali Nayebi <elderekkoo@gmail.com>
2023-09-29 17:02:45 +0330

    new form format

---
+++ note app/src/NewNote.tsx
@@ -0,0 +1,43 @@
+import { useRef } from 'react';
+import { Button, Col, Form, Row, Stack } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import CreatableReactSelect from 'react-select/creatable';
+
+export function NewNote() {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const markdownRef = useRef<HTMLTextAreaElement>(null);
+  return (
+    <Form>
+      <Stack gap={4}>
+        <Row>
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control ref={titleRef} required />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <CreatableReactSelect isMulti />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Form.Group controlId="markdown">
+          <Form.Label>Body</Form.Label>
+          <Form.Control required as="textarea" ref={markdownRef} rows={15} />
+        </Form.Group>
+        <Stack direction="horizontal" gap={2} className="justify-content-end">
+          <Button type="submit" variant="primary">
+            Save
+          </Button>
+          <Link to="..">
+            <Button type="submit" variant="outline-secondary">
+              Cancel
+            </Button>
+          </Link>
+        </Stack>
+      </Stack>
+    </Form>
+  );
+}
